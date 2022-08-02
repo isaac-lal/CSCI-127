b23a5b31dd1be46537d2196a07d96fbf90f57f99
isaaclal124 <isaaclal124@gmail.com>
2022-08-02 15:57:22 -0400

    modified hello-world.py

--- Programming Assignments/hello-world.py
+++ Programming Assignments/hello-world.py
@@ -1,6 +1,6 @@
 # Name: Isaac Lal
 # Email: isaac.lal46@myhunter.cuny.edu
-# Date: August 1, 2022
+# Date: February 14, 2022
 # Programming Assignment #1: Write a program that prints "Hello, World!" to the screen.
 
 print("Hello, World!")

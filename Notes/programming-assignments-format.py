1680f76bf407abb2b25e9fa0205ec38a8cbf442a
isaaclal124 <isaaclal124@gmail.com>
2022-08-22 18:45:43 -0400

    k

--- Notes/programming-assignments-format.py
+++ Notes/programming-assignments-format.py
@@ -1,4 +1,4 @@
 # Name: Isaac Lal
 # Email: isaac.lal46@myhunter.cuny.edu
-# Date: August 1, 2022
-# Programming Assignment #: This is the format for all assignments
+# Date: August 25, 2021
+# Programming Assignment #
